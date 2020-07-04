1ffd90fb8308bc64fbd49950650728abb7993b5e
OsamaQadi <47371098+OsamaQadi@users.noreply.github.com>
2020-07-04 12:53:51 +0300

    initial commit

---
+++ script99.js
@@ -0,0 +1,19 @@
+function puri() {
+    alert("Unavailable");
+}
+
+function kabab() {
+    alert("Available");
+}
+
+function nihari() {
+    alert("Available");
+}
+
+function beefBurger() {
+    alert("Unavailable");
+}
+
+
+
+
